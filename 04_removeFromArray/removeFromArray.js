822528b647c13bed28d175551a24feb60bd11b63
Jackie Nguyen <jackie_nguyen@outlook.com>
2022-07-21 23:15:46 -0700

    Complete js excercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    firstArray = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        for (let j = firstArray.length - 1; j >= 0; j--) {
+            if (firstArray[j] === arguments[i]) {
+                firstArray.splice(j,1);
+            }
+        }
+    }
+    return firstArray;
 };
 
 // Do not edit below this line
