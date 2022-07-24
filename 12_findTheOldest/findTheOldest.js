87d1ef229cac27c4749b2661485da041eca477e3
Jackie Nguyen <jackie_nguyen@outlook.com>
2022-07-24 00:52:08 -0700

    Complete js excercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson;
+    let oldestAge = 0;
+    const currentYear = new Date().getFullYear();
 
+    for (let i = 0; i < people.length; i++) {
+        if (!people[i].hasOwnProperty('yearOfDeath')) {
+            people[i].yearOfDeath = currentYear;
+        }
+        if (people[i].yearOfDeath - people[i].yearOfBirth > oldestAge) {
+            oldestPerson = people[i];
+            oldestAge = people[i].yearOfDeath - people[i].yearOfBirth;
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
