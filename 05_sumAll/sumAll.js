c65fbd136f4effc18bef584066f9572ef077d485
Jackie Nguyen <jackie_nguyen@outlook.com>
2022-07-23 22:05:39 -0700

    Complete js excercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
 
+    const largerNum = Math.max(num1, num2);
+    const smallerNum = Math.min(num1, num2);
+    let total = 0;
+
+    for (let i = smallerNum; i <= largerNum; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
