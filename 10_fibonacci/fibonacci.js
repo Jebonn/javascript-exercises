3802d688a92ac3f950ce0f66d28a8f33d899148d
Jackie Nguyen <jackie_nguyen@outlook.com>
2022-07-24 00:09:41 -0700

    Complete js excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
+    if (num < 0) {
+        return "OOPS";
+    }
 
+    let first_num = 1;
+    let last_num = 1;
+    let temp_num = 0;
+
+    while (num > 2) {
+        temp_num = first_num + last_num;
+        first_num = last_num;
+        last_num = temp_num;
+        num--;
+    }
+    return last_num;
 };
 
 // Do not edit below this line
